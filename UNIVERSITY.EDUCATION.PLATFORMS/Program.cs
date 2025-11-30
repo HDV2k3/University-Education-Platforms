cf3d24009e276aa325bb7f760c49b9b624ddaf0a
Huynh Dac Viet <HDV2k3@users.noreply.github.com>
2025-11-30 16:01:12 +0700

    feat: swagger UI

--- UNIVERSITY.EDUCATION.PLATFORMS/Program.cs
+++ UNIVERSITY.EDUCATION.PLATFORMS/Program.cs
@@ -1,23 +1,56 @@
-using Microsoft.OpenApi;
+﻿using Microsoft.OpenApi;
 using UNIVERSITY.EDUCATION.PLATFORMS.Application.Extensions;
 using UNIVERSITY.EDUCATION.PLATFORMS.Infrastructure.Extensions;
 using UNIVERSITY.EDUCATION.PLATFORMS.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors();
+
+// CORS
+builder.Services.AddCors(o =>
+{
+    o.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+// Register Layers
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+// Controllers
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "University Education Platforms v1.0", Version = "v1.0" });
-    c.OperationFilter<SwaggerHeaderFilter>();
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "University Education Platforms v1.0",
+        Version = "v1.0"
+    });
+    //c.OperationFilter<SwaggerHeaderFilter>();
 });
+
 builder.AddServiceDefaults();
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/health", () => Results.Ok(new { status = "Healthy-test-cicd", timestamp = DateTime.UtcNow }));
+// Swagger Middleware
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UEP API v1");
+    c.RoutePrefix = string.Empty; 
+});
+
+// Routing
+app.UseAuthorization();
+app.MapControllers();
+
+// Test endpoints
+app.MapGet("/", () => "UEP API is running (.NET 9)");
+app.MapGet("/health", () =>
+    Results.Ok(new { status = "Healthy", timestamp = DateTime.UtcNow }));
+
 app.Run();
