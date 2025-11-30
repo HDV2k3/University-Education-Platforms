c920bfc6de20d03e40a029d0af0a1dfae0a8b540
Huynh Dac Viet <HDV2k3@users.noreply.github.com>
2025-11-30 17:45:32 +0700

    fix : final fix cicd and deploy v1

--- UNIVERSITY.EDUCATION.PLATFORMS/Controllers/UsersController.cs
+++ UNIVERSITY.EDUCATION.PLATFORMS/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UNIVERSITY.EDUCATION.PLATFORMS.Application.Models.Users;
 using UNIVERSITY.EDUCATION.PLATFORMS.Application.Services.Interface;
 
